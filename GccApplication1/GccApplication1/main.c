fda8b7e844c167e082ba4644f8f3f51c87866ba9
engineershkim <gimseonghan9@gmail.com>
2024-10-02 15:40:02 +0900

    1

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2024-10-02 오후 3:09:05
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
