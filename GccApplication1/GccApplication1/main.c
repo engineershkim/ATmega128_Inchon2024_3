756c6e7373336618e5cc8a281a471c2a2a3cfba9
engineershkim <gimseonghan9@gmail.com>
2024-10-02 16:53:45 +0900

    3

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -2,18 +2,20 @@
  * GccApplication1.c
  *
  * Created: 2024-10-02 오후 3:09:05
- * Author : user
+ * Author : Engineer shkim
  */ 
 
 #include <avr/io.h>
-
-
+// LED Port : PG4
 int main(void)
 {
     /* Replace with your application code */
+	// printf("Hello World");
+	DDRG |= 0x10; // xxx1 0000 : 1=출력, 0=입력
+	
     while (1) 
     {
-		printf("hello github");
+		PORTG |= 0x10;
     }
 }
 
